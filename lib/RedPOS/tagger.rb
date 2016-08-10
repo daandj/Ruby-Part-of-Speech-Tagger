625f2e236430cd5ee116d051452de994091c33fe
Daan de Jong <daanvulkaan@gmail.com>
2016-08-11 00:14:55 +0200

    Remove empty pre_process() method.

--- lib/RedPOS/tagger.rb
+++ lib/RedPOS/tagger.rb
@@ -1,67 +1,63 @@
 module RedPOS
-  class Tagger
-    attr_accessor :classes, :model
-    
-    def initialize()
-      @classes = nil # TODO: retrieve the correct classes somehow.
-      @model = Perceptron.new(@classes)
-    end
-    
-    def tag(sentences)
-      
-    end
-    
-    def pre_process()
-      
-    end
-    
-    def train(iterations, sentences, tags)
-      iterations.times do |iter|
-        sentences.each_with_index do |sent, sent_i|
-	  context = [:START, :START2] + sent + [:END, :END2]
-	  last_tag, secondlast_tag = :START2, :START 
-          sent.each_with_index do |word, word_i|
-            features = get_features(word_i+2, context, last_tag, secondlast_tag)
-            
-            prediction = @model.predict(features)
-            true_tag = tags[sent_i][word_i]
-            
-            if prediction != true_tag
-              update(prediction, features, true_tag)
-            end
-          end
-        end
-      end
-    end
-    
-    def get_features(i, sentence, last_tag, secondlast_tag)
-      features = Hash.new(0)
-      
-      features["Wi: #{sentence[i]}"] = 1
-      features["Wi-1: #{sentence[i-1]}"] = 1
-      features["Wi-2: #{sentence[i-2]}"] = 1
-      features["Wi+1: #{sentence[i+1]}"] = 1
-      features["Wi+2: #{sentence[i+2]}"] = 1
-      features["Ti-1: #{last_tag}"] = 1
-      features["Ti-2: #{secondlast_tag}"] = 1
-      features["Ti-1, Ti-2: #{last_tag}, #{secondlast_tag}"] = 1
-      features["Wi-1 suffix: #{sentence[i][0, 3]}"] = 1
-      features["Wi-1 prefix: #{sentence[i][-3, 3]}"] = 1
-      features["Contains hyphen"] = 1 if sentence[i]["-"]
-      features["Contains number"] = 1 if sentence[i][/\d/]
-      return features
-    end
-    
-    private
-    
-    def update(prediction, features, true_tag)
-      @model.weigths.each do |feature, classes|
-        if features.include?(feature)  
-          @model.weigths[feature][prediction] -= 1
-          @model.weigths[feature][true_tag] += 1
-        end
-      end
-    end
-    
-  end
+	class Tagger
+		attr_accessor :classes, :model
+	
+		def initialize()
+			@classes = nil # TODO: retrieve the correct classes somehow.
+			@model = Perceptron.new(@classes)
+		end
+	
+		def tag(sentences)
+	
+		end
+	
+		def train(iterations, sentences, tags)
+			iterations.times do |iter|
+				sentences.each_with_index do |sent, sent_i|
+					context = [:START, :START2] + sent + [:END, :END2]
+					last_tag, secondlast_tag = :START2, :START
+					sent.each_with_index do |word, word_i|
+						features = get_features(word_i+2, context, last_tag, secondlast_tag)
+
+						prediction = @model.predict(features)
+						true_tag = tags[sent_i][word_i]
+	
+						if prediction != true_tag
+							update(prediction, features, true_tag)
+						end
+					end
+				end
+			end
+		end
+	
+		def get_features(i, sentence, last_tag, secondlast_tag)
+			features = Hash.new(0)
+	
+			features["Wi: #{sentence[i]}"] = 1
+			features["Wi-1: #{sentence[i-1]}"] = 1
+			features["Wi-2: #{sentence[i-2]}"] = 1
+			features["Wi+1: #{sentence[i+1]}"] = 1
+			features["Wi+2: #{sentence[i+2]}"] = 1
+			features["Ti-1: #{last_tag}"] = 1
+			features["Ti-2: #{secondlast_tag}"] = 1
+			features["Ti-1, Ti-2: #{last_tag}, #{secondlast_tag}"] = 1
+			features["Wi-1 suffix: #{sentence[i][0, 3]}"] = 1
+			features["Wi-1 prefix: #{sentence[i][-3, 3]}"] = 1
+			features["Contains hyphen"] = 1 if sentence[i]["-"]
+			features["Contains number"] = 1 if sentence[i][/\d/]
+			return features
+		end
+	
+		private
+	
+		def update(prediction, features, true_tag)
+			@model.weigths.each do |feature, classes|
+				if features.include?(feature)  
+					@model.weigths[feature][prediction] -= 1
+					@model.weigths[feature][true_tag] += 1
+				end
+			end
+		end
+	
+	end
 end
